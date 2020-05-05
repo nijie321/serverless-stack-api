ebe478b61eb09c75739d620718168d7954b1859a
Jie Ni <nijie321@outlook.com>
2020-05-04 21:51:22 -0500

    add unit test

--- billing.js
+++ billing.js
@@ -6,9 +6,7 @@ export const main = handler(async (event, context) => {
     const {storage, source} = JSON.parse(event.body);
     const amount = calculateCost(storage);
     const description = "Scratch charge";
-
     const stripe = stripePackage(process.env.stripeSecretKey);
-    
     await stripe.charges.create({
         source,
         amount,
